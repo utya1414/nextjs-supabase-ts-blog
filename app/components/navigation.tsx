f142dcaf1272694c732d5acac04196cfa087c2bf
utya1414 <utya1414@gmail.com>
2023-11-12 11:16:17 +0900

    sessionのバグを直しました。

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Link from "next/link";
 import type { Session } from "@supabase/auth-helpers-nextjs";
 
-const Navigation = (session: { session: Session | null }) => {
+const Navigation = ({ session }: { session: Session | null }) => {
   return (
     <header className="shadow-lg shadow-gray-100">
       <div className=" flex justify-between items-center py-5 container mx-auto">
@@ -12,16 +12,20 @@ const Navigation = (session: { session: Session | null }) => {
           My Blog
         </Link>
         <div className="font-bold text-md">
-        {!session ? (
-          <div className="cursor-pointer">
-            <Link href="/">ログイン中</Link>
-          </div>
-        ) : (
-          <div className="flex items-center space-x-5">
-            <Link href="auth/login" className="cursor-pointer">ログイン</Link>
-            <Link href="auth/signup" className="cursor-pointer">サインアップ</Link>
-          </div>
-        )}
+          {session ? (
+            <div className="cursor-pointer">
+              <Link href="/">ログイン中</Link>
+            </div>
+          ) : (
+            <div className="flex items-center space-x-5">
+              <Link href="auth/login" className="cursor-pointer">
+                ログイン
+              </Link>
+              <Link href="auth/signup" className="cursor-pointer">
+                サインアップ
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </header>
