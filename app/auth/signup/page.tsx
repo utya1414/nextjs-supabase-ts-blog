b113be2c5a15e9648be9a1415e747eda1831267a
utya1414 <utya1414@gmail.com>
2023-11-12 19:27:08 +0900

    サーバークライアントののサインアップルートハンドラを作成しました。

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,20 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Signup from "@/app/components/signup";
+import { redirect } from "next/navigation";
+
+import type { Database } from "@/lib/database.types";
+
+const SignUp = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (session) {
+    redirect("/");
+  }
+
+  return <Signup />;
+};
+
+export default SignUp;
