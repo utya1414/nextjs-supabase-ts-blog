43bf0bd417e963e8246c8b831793bf8c24dff8d3
utya1414 <utya1414@gmail.com>
2023-11-12 12:41:09 +0900

    ログイン機能の√ハンドラを作成しました。

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,22 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import type { Database } from "@/lib/database.types";
+import { redirect } from "next/navigation";
+import Login from "@/app/components/login";
+
+// ログインページ
+const LoginPage = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect("/");
+  }
+
+  return <Login />;
+};
+
+export default LoginPage;
