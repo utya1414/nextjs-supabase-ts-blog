698253884e42906b8f04c0e400232599e319da0c
utya1414 <utya1414@gmail.com>
2023-11-12 19:28:37 +0900

    サインアップ機能のクライアントコンポーネントを作成しました

---
+++ app/components/signup.tsx
@@ -0,0 +1,122 @@
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Loading from "@/app/loading";
+import { useState } from "react";
+
+import type { Database } from "@/lib/database.types";
+type Schema = z.infer<typeof schema>;
+
+const schema = z.object({
+  name: z.string().min(1, { message: "名前を入力してください" }),
+  email: z
+    .string()
+    .email({ message: "メールアドレスの形式で入力してください" }),
+  password: z
+    .string()
+    .min(8, { message: "パスワードは8文字以上で入力してください" }),
+});
+const Signup = () => {
+  const supabase = createClientComponentClient<Database>();
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [sendMessage, setSendMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Schema>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data: Schema) => {
+    setLoading(true);
+    try {
+      const { error: errorSignup } = await supabase.auth.signUp({
+        email: data.email,
+        password: data.password,
+        options: {
+          emailRedirectTo: `${location.origin}/auth/callback`,
+        },
+      });
+
+      if (errorSignup) {
+        setMessage("エラーが発生しました" + errorSignup);
+        return;
+      }
+
+      const { error: updateError } = await supabase
+        .from("users")
+        .update({
+          name: data.name,
+        })
+        .eq("email", data.email);
+      if (updateError) {
+        setMessage("エラーが発生しました" + updateError);
+        return;
+      }
+
+      reset();
+      setSendMessage("確認メールを送信しました");
+    } catch (error) {
+      setMessage("エラーが発生しました" + error);
+    } finally {
+      setLoading(false);
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col items-center my-5">
+          <div className="font-bold text-xl">サインアップ</div>
+          <input
+            type="text"
+            className="border rounded-md focus:outline-none my-2 text-lg"
+            placeholder="名前"
+            id="name"
+            {...register("name", { required: true })}
+          ></input>
+          <input
+            type="email"
+            className="border rounded-md focus:outline-none my-2 text-lg "
+            placeholder="メールアドレス"
+            id="email"
+            {...register("email", { required: true })}
+          ></input>
+          <input
+            type="password"
+            className="border rounded-md focus:outline-none my-2 text-lg"
+            placeholder="パスワード"
+            id="password"
+            {...register("password", { required: true })}
+          ></input>
+          {message && <div className="text-red-500 text-sm">{message}</div>}
+          {sendMessage && <div className="text-green-500 text-sm">{sendMessage}</div>}
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              type="submit"
+              className="font-bold my-5 py-2 px-2 border rounded-lg text-white bg-sky-500 shadow-lg hover:bg-sky-600 "
+            >
+              確認メール送信
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Signup;
