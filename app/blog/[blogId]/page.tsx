69f484a55d5fd4bbd324cccd866afe17ad1533e1
utya1414 <utya1414@gmail.com>
2023-11-13 22:51:02 +0900

    ブログ詳細画面と削除機能を作成しました

---
+++ app/blog/[blogId]/page.tsx
@@ -0,0 +1,46 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import type { BlogListType } from "@/lib/blog.types";
+import BlogDetail from "@/app/components/blog/blog-detail";
+import { Database } from "@/lib/database.types";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+
+type PageProps = {
+  params: {
+    blogId: string;
+  };
+};
+const PageToBlogDetail = async ({ params }: PageProps) => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+
+  const { data: blogData } = await supabase
+    .from("blogs")
+    .select()
+    .eq("id", params.blogId)
+    .single();
+
+  if (!blogData) {
+    return notFound();
+  }
+
+  const { data: userData } = await supabase
+    .from("users")
+    .select()
+    .eq("id", blogData.user_id)
+    .single();
+  if (!userData) {
+    return notFound();
+  }
+  const blog: BlogListType = {
+    id: blogData.id,
+    created_at: blogData.created_at,
+    title: blogData.title,
+    content: blogData.content,
+    user_id: blogData.user_id,
+    image_url: blogData.image_url,
+    name: userData.name,
+  };
+  return <BlogDetail {...blog} />;
+};
+
+export default PageToBlogDetail;
