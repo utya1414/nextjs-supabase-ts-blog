d85ca8334feadf899ba737dd3a60ad988bbb38c5
utya1414 <utya1414@gmail.com>
2023-11-13 19:41:51 +0900

    ブログリストにcssをあてました

--- app/components/blog/blog-item.tsx
+++ app/components/blog/blog-item.tsx
@@ -14,18 +14,19 @@ const BlogItem = (blog: BlogListType) => {
 
   const date = format(new Date(blog.created_at), "yyyy/MM/dd");
   return (
-    <div>
+    <Link href={`/blog/${blog.id}`} className="bg-gray-200 py-2 px-2 rounded-md shadow-md hover:brightness-90 text-sm">
       <Image
         src={blog.image_url}
         alt="image"
-        className=""
+        className="rounded-md"
         width={300}
         height={200}
       />
-      <div>{blog.title}</div>
-      <div>by {blog.name}</div>
-      <div>{date}</div>
-    </div>
+      <div className="text-gray-500 font-bold">{date}</div>
+      <div className="font-bold text-lg">{blog.title}</div>
+      <div className="text-gray-500 font-bold">{blog.content}</div>
+      <div className="font-bold">{blog.name}</div>
+    </Link>
   );
 };
 
