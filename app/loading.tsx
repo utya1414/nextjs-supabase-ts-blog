6e60d94728bc4c9efedf4c760b04fdc2396a28dd
utya1414 <utya1414@gmail.com>
2023-11-18 19:28:49 +0900

    コメントテーブルといいねテーブルとプロフィールテーブルを追加しました

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@
 const Loading = () => {
     return (
         <div className="flex justify-center">
-            <div className="h-7 w-7 animate-spin rounded-full border-2 border-sky-500 border-t-transparent"></div> 
+            <div className="h-7 w-7 animate-spin rounded-full border-2 border-secondary border-t-transparent"></div> 
         </div>
     )
 }
