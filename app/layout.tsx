2ee7c2dfeefdb1c12f4b3f763398cd9201252884
utya1414 <utya1414@gmail.com>
2023-12-10 20:07:56 +0900

    force-dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-export const dynamic = "auto";
+export const dynamic = "force-dynamic";
 import LoginObserver from "./components/auth/login-observer";
 
 const inter = Inter({ subsets: ["latin"] });
